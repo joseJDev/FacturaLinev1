4f865571c312e5c153d37837a3d6c5087cd77033
Juan Jose <josejuan250203@gmail.com>
2021-12-05 19:41:26 -0500

    ajustes

--- static/js/facture/ajax.js
+++ static/js/facture/ajax.js
@@ -70,3 +70,50 @@ function generateQuotes(){
         }
     });
 }
+function generateQuotesRec(){
+    // Traer productos 
+    let products = getStorage();
+    let productsArray = []
+    
+    products.forEach(product => {
+        let json = {
+            id: product.id_product,
+            count: product.amount
+        }
+        productsArray.push(json)
+    });
+
+    const arrayString = JSON.stringify(productsArray)
+    console.log(arrayString)
+    console.log(typeof(arrayString))
+
+    $.ajax({
+        data: {
+            'client': $('#idClient').val(),
+            'product': `${arrayString}`,
+            'quota': $('#quote').val(),
+            'payment': $('#payment').val(),
+            'discount': $('#discount').val(),
+            'doc_patient': $('#docPatient').val(),
+            'fullname_patient': $('#namePatient').val(),
+            'csrfmiddlewaretoken': $("[name='csrfmiddlewaretoken']").val()
+        },
+        url: '/facture-gen-quotes/',
+        type: 'POST',
+        success: function (response){
+            url = response['url'];
+            let win =  window.open(url, '_blank');
+            win.focus();
+            clearValuesInput();
+            clearStorage()
+            listTableProducts();
+            calculateTotal();
+            clearElements();
+        },
+        error: function(error){
+            viewError(error.responseJSON['message'], true)
+            //Limpiar los campos
+            //Mostrar los errores en el modal
+        }
+    });
+}
